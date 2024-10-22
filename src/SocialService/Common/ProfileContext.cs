c5c92f643d13f23e1231e06cd35451bb93ad3ec2
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-10-22 17:19:42 -0300

    feat: utils for Profile

---
+++ src/SocialService/Common/ProfileContext.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialService.Common;
+
+public static class ProfileContext
+{
+    private static readonly AsyncLocal<Guid> _profileId = new();
+    
+    public static Guid ProfileId
+    {
+        get => _profileId.Value;
+        set => _profileId.Value = value;
+    }
+}
