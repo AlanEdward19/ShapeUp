ae27fb6d1e3ecea61cf2b21138cba43038499f5d
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-10-23 16:38:19 -0300

    feat: migration

---
+++ src/SocialService/Migrations/20241023174003_NewFieldsOnProfile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialService.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldsOnProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Profiles");
+        }
+    }
+}
