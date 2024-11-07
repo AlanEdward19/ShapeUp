3ab5e53558d07b288b8de6e4a443f4b2a8d137cd
Alan Oliveira - BDS DataSolution <aoliveira@bdsdatasolution.com.br>
2024-11-06 22:16:09 -0300

    feat: follower specific mongoContext implemented

---
+++ src/SocialService/Database/Mongo/Contracts/IFollowerMongoContext.cs
@@ -0,0 +1,21 @@
+﻿using SocialService.Follow;
+
+namespace SocialService.Database.Mongo.Contracts;
+
+public interface IFollowerMongoContext
+{
+    #region Profile
+
+    Task AddProfileDocumentAsync(ProfileConnections document);
+    Task<ProfileConnections> GetProfileDocumentByIdAsync(Guid profileId);
+    Task DeleteProfileDocumentByIdAsync(Guid objectId);
+
+    #endregion
+
+    #region Follower
+
+    Task FollowProfileAsync(Guid userId, Guid followId);
+    Task UnfollowProfileAsync(Guid userId, Guid followId);
+
+    #endregion
+}
---
+++ src/SocialService/Database/Mongo/FollowerMongoContext.cs
@@ -0,0 +1,107 @@
+﻿using MongoDB.Driver;
+using SocialService.Database.Mongo.Contracts;
+using SocialService.Follow;
+using SocialService.Friends;
+
+namespace SocialService.Database.Mongo;
+
+public class FollowerMongoContext : IFollowerMongoContext
+{
+    private readonly IMongoCollection<ProfileConnections> _collection;
+    private readonly ILogger<FollowerMongoContext> _logger;
+
+    public FollowerMongoContext(string connectionString, string databaseName, string collectionName, ILogger<FollowerMongoContext> logger)
+    {
+        var client = new MongoClient(connectionString);
+        var database = client.GetDatabase(databaseName);
+        _collection = database.GetCollection<ProfileConnections>(collectionName);
+        _logger = logger;
+    }
+
+    #region Profile
+
+    public async Task AddProfileDocumentAsync(ProfileConnections document)
+    {
+        try
+        {
+            await _collection.InsertOneAsync(document);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error adding profile document.");
+            throw;
+        }
+    }
+
+    public async Task<ProfileConnections> GetProfileDocumentByIdAsync(Guid profileId)
+    {
+        try
+        {
+            var filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, profileId.ToString());
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting profile document by ID.");
+            return default;
+        }
+    }
+
+    public async Task DeleteProfileDocumentByIdAsync(Guid objectId)
+    {
+        try
+        {
+            var filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, objectId.ToString());
+            var result = await _collection.DeleteOneAsync(filter);
+            _logger.LogInformation(result.DeletedCount > 0 ? "Document deleted successfully." : "No document was deleted.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error deleting profile document.");
+        }
+    }
+
+    #endregion
+
+    #region Follower
+
+    public async Task FollowProfileAsync(Guid userId, Guid followId)
+    {
+        try
+        {
+            var filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, userId.ToString());
+            var update = Builders<ProfileConnections>.Update.Push(x => x.Following, followId.ToString());
+            await _collection.UpdateOneAsync(filter, update);
+            
+            filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, followId.ToString());
+            update = Builders<ProfileConnections>.Update.Push(x => x.Followers, userId.ToString());
+            await _collection.UpdateOneAsync(filter, update);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error following profile.");
+            throw;
+        }
+    }
+    
+    public async Task UnfollowProfileAsync(Guid userId, Guid followId)
+    {
+        try
+        {
+            var filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, userId.ToString());
+            var update = Builders<ProfileConnections>.Update.Pull(x => x.Following, followId.ToString());
+            await _collection.UpdateOneAsync(filter, update);
+            
+            filter = Builders<ProfileConnections>.Filter.Eq(x => x.ProfileId, followId.ToString());
+            update = Builders<ProfileConnections>.Update.Pull(x => x.Followers, userId.ToString());
+            await _collection.UpdateOneAsync(filter, update);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error unfollowing profile.");
+            throw;
+        }
+    }
+
+    #endregion
+}
