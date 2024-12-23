5c2b1adec28f0efb92fa3fb7a6ba19392181bda8
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2024-12-23 17:34:45 -0300

    feat: follow validations

---
+++ src/SocialService/Follow/FollowUser/FollowUserCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SocialService.Profile.Common.Repository;
+
+namespace SocialService.Follow.FollowUser;
+
+/// <summary>
+/// Validador para o comando de seguir um usuário.
+/// </summary>
+public class FollowUserCommandValidator : AbstractValidator<FollowUserCommand>
+{
+    /// <summary>
+    /// Validações para o comando de seguir um usuário.
+    /// </summary>
+    /// <param name="repository"></param>
+    public FollowUserCommandValidator(IProfileGraphRepository repository)
+    {
+        RuleFor(x => x.FollowedUserId)
+            .MustAsync(async (followedUserId, cancellationToken) => await repository.ProfileExistsAsync(followedUserId))
+            .WithMessage("FollowedUserId: '{PropertyValue}' doesn't exist.");
+    }
+}
---
+++ src/SocialService/Follow/UnfollowUser/UnfollowUserCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SocialService.Profile.Common.Repository;
+
+namespace SocialService.Follow.UnfollowUser;
+
+/// <summary>
+/// Validador para o comando de parar de seguir um usuário.
+/// </summary>
+public class UnfollowUserCommandValidator : AbstractValidator<UnfollowUserCommand>
+{
+    /// <summary>
+    /// Validações para o comando de parar de seguir um usuário.
+    /// </summary>
+    /// <param name="repository"></param>
+    public UnfollowUserCommandValidator(IProfileGraphRepository repository)
+    {
+        RuleFor(x => x.UnfollowedUserId)
+            .MustAsync(async (followedUserId, cancellationToken) => await repository.ProfileExistsAsync(followedUserId))
+            .WithMessage("UnfollowedUserId: '{PropertyValue}' doesn't exist.");
+    }
+}
