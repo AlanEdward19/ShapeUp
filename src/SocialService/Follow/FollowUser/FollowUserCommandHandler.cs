199349496f127992b35efb8ce6fe50bf9d4b7d1c
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2025-01-28 20:44:02 -0300

    feat: notification on newFollower

--- src/SocialService/Follow/FollowUser/FollowUserCommandHandler.cs
+++ src/SocialService/Follow/FollowUser/FollowUserCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using SocialService.Follow.Common.Repository;
+﻿using SocialService.Common.Enums;
+using SocialService.Common.Events;
+using SocialService.Common.Services;
+using SocialService.Follow.Common.Repository;
+using SocialService.Profile.Common.Repository;
 
 namespace SocialService.Follow.FollowUser;
 
@@ -6,7 +10,7 @@ namespace SocialService.Follow.FollowUser;
 ///     Handler para seguir um usuário.
 /// </summary>
 /// <param name="graphRepository"></param>
-public class FollowUserCommandHandler(IFollowerGraphRepository graphRepository) : IHandler<bool, FollowUserCommand>
+public class FollowUserCommandHandler(IFollowerGraphRepository graphRepository, IProfileGraphRepository profileGraphRepository, INotificationPublisher notificationPublisher) : IHandler<bool, FollowUserCommand>
 {
     /// <summary>
     ///     Método para seguir um usuário.
@@ -17,6 +21,16 @@ public class FollowUserCommandHandler(IFollowerGraphRepository graphRepository) 
     public async Task<bool> HandleAsync(FollowUserCommand command, CancellationToken cancellationToken)
     {
         await graphRepository.FollowAsync(ProfileContext.ProfileId, command.FollowedUserId);
+        Profile.Profile profile = await profileGraphRepository.GetProfileAsync(ProfileContext.ProfileId);
+        
+        NotificationEvent @event = new()
+        {
+            RecipientId = command.FollowedUserId,
+            Topic = ENotificationTopic.NewFollower,
+            Content = $"{profile.FirstName} {profile.LastName} começou a te seguir.",
+        };
+
+        await notificationPublisher.PublishNotificationEventAsync(@event);
 
         return true;
     }
