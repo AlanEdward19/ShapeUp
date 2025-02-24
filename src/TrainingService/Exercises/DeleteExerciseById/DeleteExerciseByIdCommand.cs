756bba6af7b46ab5585d7a5f4e62fcc290b51b9b
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2025-02-24 20:51:28 -0300

    feat: implement exercise commands and queries for CRUD operations

---
+++ src/TrainingService/Exercises/DeleteExerciseById/DeleteExerciseByIdCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace TrainingService.Exercises.DeleteExerciseById;
+
+/// <summary>
+/// Comando para deletar um exercício.
+/// </summary>
+/// <param name="id"></param>
+public class DeleteExerciseByIdCommand(Guid id)
+{
+    /// <summary>
+    /// Id do exercício.
+    /// </summary>
+    public Guid Id { get; set; } = id;
+}
---
+++ src/TrainingService/Exercises/UpdateExercise/UpdateExerciseCommand.cs
@@ -0,0 +1,33 @@
+﻿using TrainingService.Exercises.Common.Enums;
+
+namespace TrainingService.Exercises.UpdateExercise;
+
+/// <summary>
+/// Comando para atualizar um exercício.
+/// </summary>
+/// <param name="id"></param>
+/// <param name="name"></param>
+/// <param name="muscleGroups"></param>
+/// <param name="requiresWeight"></param>
+public class UpdateExerciseCommand(Guid id, string? name, IEnumerable<EMuscleGroup>? muscleGroups, bool? requiresWeight)
+{
+    /// <summary>
+    /// Id do exercício.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Nome do exercício.
+    /// </summary>
+    public string? Name { get; set; } = name;
+
+    /// <summary>
+    /// Grupos musculares ao qual o exercício pertence.
+    /// </summary>
+    public IEnumerable<EMuscleGroup>? MuscleGroups { get; set; } = muscleGroups;
+
+    /// <summary>
+    /// Se o exercício requer peso.
+    /// </summary>
+    public bool? RequiresWeight { get; set; } = requiresWeight;
+}
