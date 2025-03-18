1026bb74d86ffb54fdcf09db01db451c90ec74b2
AlanEdward19 <alan-edward@outlook.com.br>
2025-03-18 11:42:30 -0300

    feat: update project references and configure CORS policy in Program.cs

--- src/AuthService/Program.cs
+++ src/AuthService/Program.cs
@@ -1,23 +1,36 @@
+using System.Globalization;
+using AuthService.Configuration;
+using ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-// Add services to the container.
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost8080",
+        builder => builder.WithOrigins("http://localhost:8080")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+builder.Services.SolveServiceDependencies(configuration);
+builder.Services.ConfigureAuthentication(configuration);
+builder.Services.ConfigureEndpoints();
+
+var cultureInfo = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Use CORS policy
+app.UseCors("AllowLocalhost8080");
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.ConfigureSwagger();
+app.MapEndpoints(configuration);
+app.ConfigureMiddleware();
 
 app.Run();
+app.Logger.LogInformation("Application instance is ready to handle incoming requests");
