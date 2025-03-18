98cdd7bad91885207c92b7023a8a151d244b3ade
AlanEdward19 <alan-edward@outlook.com.br>
2025-03-18 16:28:16 -0300

    feat: implement GroupController with CRUD operations and user management

---
+++ src/AuthService/Group/GroupController.cs
@@ -0,0 +1,68 @@
+﻿using Asp.Versioning;
+using AuthService.Authentication.AuthenticateUser;
+using AuthService.Authentication.Common.Enums;
+using AuthService.Common.Interfaces;
+using AuthService.Common.User;
+using AuthService.Group.AddUserToGroup;
+using AuthService.Group.CreateGroup;
+using AuthService.Group.DeleteGroup;
+using AuthService.Group.GetUsersFromGroup;
+using AuthService.Group.RemoveUserFromGroup;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Group;
+
+[ApiVersion("1.0")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("v{version:apiVersion}/[Controller]")]
+public class GroupController : ControllerBase
+{
+    [HttpPost("{groupId:guid}/Users/{userId:guid}")]
+    public async Task<IActionResult> AddUserToGroup(Guid groupId, Guid userId, [FromBody] EGroupRole role,
+        [FromServices] IHandler<string, AddUserToGroupCommand> handler, CancellationToken cancellationToken)
+    {
+        AddUserToGroupCommand command = new(groupId, userId, role);
+        var token = await handler.HandleAsync(command, cancellationToken);
+        return Ok(new { Token = token });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateGroup([FromBody] CreateGroupCommand command,
+        [FromServices] IHandler<bool, CreateGroupCommand> handler, CancellationToken cancellationToken)
+    {
+        await handler.HandleAsync(command, cancellationToken);
+
+        return Created();
+    }
+    
+    [HttpDelete("{groupId:guid}")]
+    public async Task<IActionResult> DeleteGroup(Guid groupId,
+        [FromServices] IHandler<bool, DeleteGroupCommand> handler, CancellationToken cancellationToken)
+    {
+        DeleteGroupCommand command = new(groupId);
+        await handler.HandleAsync(command, cancellationToken);
+
+        return NoContent();
+    }
+    
+    [HttpGet("{groupId:guid}/Users")]
+    public async Task<IActionResult> GetUsersFromGroup(Guid groupId,
+        [FromServices] IHandler<ICollection<UserDto>, GetUsersFromGroupQuery> handler, CancellationToken cancellationToken)
+    {
+        GetUsersFromGroupQuery query = new(groupId);
+        return Ok(await handler.HandleAsync(query, cancellationToken));
+    }
+    
+    [HttpDelete("{groupId:guid}/Users/{userId:guid}")]
+    public async Task<IActionResult> RemoveUserFromGroup(Guid groupId, Guid userId,
+        [FromServices] IHandler<bool, RemoveUserFromGroupCommand> handler, CancellationToken cancellationToken)
+    {
+        RemoveUserFromGroupCommand command = new(groupId, userId);
+        await handler.HandleAsync(command, cancellationToken);
+
+        return NoContent();
+    }
+}
