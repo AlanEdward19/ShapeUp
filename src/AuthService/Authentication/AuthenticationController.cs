82730cdf639fd5ac9f278322efc4cacb4d66ed93
AlanEdward19 <alan-edward@outlook.com.br>
2025-03-18 11:43:33 -0300

    feat: add API versioning and authorization to AuthenticationController

--- src/AuthService/Authentication/AuthenticationController.cs
+++ src/AuthService/Authentication/AuthenticationController.cs
@@ -1,9 +1,16 @@
-﻿using AuthService.Authentication.AuthenticateUser;
+﻿using Asp.Versioning;
+using AuthService.Authentication.AuthenticateUser;
 using AuthService.Common.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthService.Authentication;
 
+[ApiVersion("1.0")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("v{version:apiVersion}/[Controller]")]
 public class AuthenticationController : ControllerBase
 {
     [HttpPost("authenticate")]
