f5c7ef7d60a0c5ef5827abf1fb4b414439d8250d
Conrado Setti <conrado.setti7@gmail.com>
2025-05-14 15:11:54 -0300

    refactor Program.cs

--- src/Orquestrator/Program.cs
+++ src/Orquestrator/Program.cs
@@ -1,59 +1,66 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
- var mongo = builder
-         .AddMongoDB("Mongo")
-         .WithDataVolume();
+var mongo = builder
+    .AddMongoDB("Mongo")
+    .WithDataVolume();
 
-// var redis = builder
-//         .AddRedis("Redis")
-//         .WithDataVolume();
-//
-// var sqlServer = builder
-//         .AddSqlServer("SqlServer")
-//         .WithDataVolume();
-//
-// var storage = builder
-//         .AddAzureStorage("Storage")
-//         .RunAsEmulator(c => c
-//                 .WithDataVolume()
-//                 .WithImageTag("latest")
-//         );
-//
-// var search = builder.AddConnectionString("Search");
-//
-// var socialService = builder
-//         .AddProject<Projects.SocialService>("SocialService")
-//         .WaitFor(redis)
-//         .WaitFor(storage)
-//         // .WaitFor(search)
-//         .WithReference(storage.AddBlobs("BlobStorage"))
-//         .WithReference(redis)
-//         .WithReference(search);
-//
-// var notificationService = builder
-//         .AddProject<Projects.NotificationService>("NotificationService")
-//         .WaitFor(redis)
-//         .WithReference(redis)
-//         .WithExternalHttpEndpoints();
-//
-// var chatService = builder
-//         .AddProject<Projects.ChatService>("ChatService")
-//         .WaitFor(redis)
-//         .WaitFor(mongo)
-//         .WithReference(redis)
-//         .WithReference(mongo)
-//         .WithExternalHttpEndpoints();
-//
- var nutritionService = builder
-         .AddProject<Projects.NutritionService>("NutritionService")
-         .WaitFor(mongo)
-         .WithReference(mongo)
-         .WithExternalHttpEndpoints();
+var redis = builder
+    .AddRedis("Redis")
+    .WithDataVolume();
 
-// var authService = builder
-//         .AddProject<Projects.AuthService>("AuthService")
-//         .WaitFor(sqlServer)
-//         .WithReference(sqlServer)
-//         .WithExternalHttpEndpoints();
+var sqlServer = builder
+    .AddSqlServer("SqlServer")
+    .WithDataVolume();
+
+var storage = builder
+    .AddAzureStorage("Storage")
+    .RunAsEmulator(c => c
+        .WithDataVolume()
+        .WithImageTag("latest")
+    );
+
+var search = builder.AddConnectionString("Search");
+
+var socialService = builder
+    .AddProject<Projects.SocialService>("SocialService")
+    .WaitFor(redis)
+    .WaitFor(storage)
+    // .WaitFor(search)
+    .WithReference(storage.AddBlobs("BlobStorage"))
+    .WithReference(redis)
+    .WithReference(search);
+
+var notificationService = builder
+    .AddProject<Projects.NotificationService>("NotificationService")
+    .WaitFor(mongo)
+    .WithReference(mongo)
+    .WithExternalHttpEndpoints();
+
+var chatService = builder
+    .AddProject<Projects.ChatService>("ChatService")
+    .WaitFor(redis)
+    .WaitFor(mongo)
+    .WithReference(redis)
+    .WithReference(mongo)
+    .WithExternalHttpEndpoints();
+
+var nutritionService = builder
+    .AddProject<Projects.NutritionService>("NutritionService")
+    .WaitFor(mongo)
+    .WithReference(mongo)
+    .WithExternalHttpEndpoints();
+
+var authService = builder
+    .AddProject<Projects.AuthService>("AuthService")
+    .WaitFor(sqlServer)
+    .WithReference(sqlServer)
+    .WithExternalHttpEndpoints();
+
+var trainingService = builder
+    .AddProject<Projects.TrainingService>("TrainingService")
+    .WaitFor(sqlServer)
+    .WaitFor(mongo)
+    .WithReference(sqlServer)
+    .WithReference(mongo);
 
 builder.Build().Run();
