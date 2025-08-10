88b2ca081a3cc33d1118ea998b3709ac43338eff
AlanEdward19 <alan-edward@outlook.com.br>
2025-08-10 15:40:53 -0300

    feat: enhance client retrieval by implementing conditional client creation and null handling

--- src/ProfessionalManagementService/Clients/GetClientById/GetClientByIdQueryHandler.cs
+++ src/ProfessionalManagementService/Clients/GetClientById/GetClientByIdQueryHandler.cs
@@ -6,9 +6,9 @@ using SharedKernel.Exceptions;
 
 namespace ProfessionalManagementService.Clients.GetClientById;
 
-public class GetClientByIdQueryHandler(DatabaseContext dbContext) : IHandler<ClientDto, GetClientByIdQuery>
+public class GetClientByIdQueryHandler(DatabaseContext dbContext) : IHandler<ClientDto?, GetClientByIdQuery>
 {
-    public async Task<ClientDto> HandleAsync(GetClientByIdQuery query, CancellationToken cancellationToken)
+    public async Task<ClientDto?> HandleAsync(GetClientByIdQuery query, CancellationToken cancellationToken)
     {
         var client = await dbContext.Clients
             .AsNoTracking()
@@ -19,7 +19,7 @@ public class GetClientByIdQueryHandler(DatabaseContext dbContext) : IHandler<Cli
             .FirstOrDefaultAsync(x => x.Id == query.Id, cancellationToken: cancellationToken);
 
         if (client == null)
-            throw new NotFoundException($"Client with Id: '{query.Id}' not found.");
+            return null;
 
         var professional = await dbContext.Professionals
             .AsNoTracking()
